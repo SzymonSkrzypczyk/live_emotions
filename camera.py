d01ae902ffbe2b1f842c4d8a1f1b30b54422c6a9
szymon <Szymon.Skrzypczyk@vp.pl>
2023-04-16 12:36:58 +0200

    first commit

---
+++ camera.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import cv2
+# noinspection PyUnresolvedReferences
+vid = cv2.VideoCapture(0)
+# noinspection PyUnresolvedReferences
+cascade = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+# noinspection PyUnresolvedReferences
+face_cascade = cv2.CascadeClassifier(cascade)
+
+while True:
+    ret, frame = vid.read()
+
+    # noinspection PyUnresolvedReferences
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=4, minSize=(40, 40))
+    for x, y, w, h in faces:
+        # noinspection PyUnresolvedReferences
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    # noinspection PyUnresolvedReferences
+    cv2.imshow('frame', frame)
+
+    # noinspection PyUnresolvedReferences
+    if cv2.waitKey(1) and 0xFF == ord("q"):
+        break
+vid.release()
+# noinspection PyUnresolvedReferences
+cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from keras import models
+from keras import layers
+from keras.utils import image_dataset_from_directory
+import pandas as pd
+"""
+0 - Angry
+1 - Disgust
+2 - Fear
+3 - Happy
+4 - Sad
+5 - Surprise
+6 - Neutral
+"""
+
+TEST_IMAGES = Path(__file__).parent / 'images' / 'test'
+TRAIN_IMAGES = Path(__file__).parent / 'images' / 'train'
+VAL_IMAGES = Path(__file__).parent / 'images' / 'val'
+
+
+def get_data():
+    train = image_dataset_from_directory(
+        TRAIN_IMAGES,
+        label_mode='categorical',
+        class_names=['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'],
+        image_size=(48, 48)
+    )
+    val = image_dataset_from_directory(
+        VAL_IMAGES,
+        label_mode='categorical',
+        class_names=['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'],
+        image_size=(48, 48)
+    )
+    test = image_dataset_from_directory(
+        TEST_IMAGES,
+        label_mode='categorical',
+        class_names=['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'],
+        image_size=(48, 48)
+    )
+    return train, val, test
+
+
+def create_model(_train, _val, _epochs: int = 10, _batch_size: int = 32):
+    _model = models.Sequential()
+    _model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 3)))
+    _model.add(layers.MaxPooling2D((2, 2)))
+    _model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    _model.add(layers.MaxPooling2D((2, 2)))
+    _model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    _model.add(layers.GlobalMaxPooling2D())
+    _model.add(layers.Flatten())
+    _model.add(layers.Dense(128, activation='relu'))
+    _model.add(layers.Dropout(.55))
+    _model.add(layers.Dense(64, activation='relu'))
+    _model.add(layers.Dense(7, activation='softmax'))
+    _model.compile(optimizer='rmsprop', metrics=['accuracy'], loss='categorical_crossentropy')
+    _model.fit(_train, epochs=_epochs, validation_data=_val, batch_size=_batch_size)
+    return _model
+
+
+if __name__ == '__main__':
+    data = get_data()
+    print(create_model(data[0], data[1]).summary())
