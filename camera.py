1a2f951751229b2b62f546b013664602f5aa99ea
szymon <Szymon.Skrzypczyk@vp.pl>
2023-04-18 16:38:22 +0200

    working reshaping and detecting

--- camera.py
+++ camera.py
@@ -1,15 +1,25 @@
 from pathlib import Path
 from io import BytesIO
 import cv2
+import numpy as np
 from PIL import Image
 from keras import models
 from keras.utils import load_img, img_to_array, array_to_img
+
+EMOTIONS = {
+    0: "Angry",
+    1: "Disgust",
+    2: "Fear",
+    3: "Happy",
+    4: "Sad",
+    5: "Surprise",
+    6: "Neutral"
+}
 
 model = models.load_model(Path(__file__).parent / 'model.h5')
 # noinspection PyUnresolvedReferences
 vid = cv2.VideoCapture(0)
 # print([i for i in dir(cv2) if 'CAP' in i])
-# noinspection PyUnresolvedReferences
 # noinspection PyUnresolvedReferences
 face_cascade = cv2.CascadeClassifier(str(Path(__file__).parent / 'haarcascade_frontalface_default.xml'))
 
@@ -29,14 +39,16 @@ while ret:
     # img = load_img(frame, target_size=(48, 48))
 
     for i in images:
-        # add coords!!!
         print(i.size)
-    '''img = array_to_img(frame).resize((48, 48))
+        # add coords!!!
+    img = array_to_img(frame).resize((48, 48))
     with BytesIO() as byte_io:
         img.save(byte_io, format='png')
-        img = load_img(byte_io, target_size=(48, 48, 3))
+        img = load_img(byte_io, target_size=(48, 48))
         img_arr = img_to_array(img)
-        print(model.predict([img_arr]))'''
+        prediction = model.predict(np.expand_dims(img_arr, axis=0), verbose=False)
+        prediction.flatten()
+        print(np.argmax(prediction))
 
     # noinspection PyUnresolvedReferences
     if cv2.waitKey(1) and 0xFF == ord("q"):
