15631564da12cfc47a01cfb0239946147e84efe6
szymon <Szymon.Skrzypczyk@vp.pl>
2023-04-20 11:05:20 +0200

    text functionality added

--- camera.py
+++ camera.py
@@ -16,6 +16,25 @@ EMOTIONS = {
     6: "Neutral"
 }
 
+
+def get_colour(_emotion: int):
+    match _emotion:
+        case 0:
+            return 0, 0, 255
+        case 1:
+            return 255, 0, 191
+        case 2:
+            return 128, 128, 128
+        case 3:
+            return 0, 255, 64
+        case 4:
+            return 255, 0, 0
+        case 5:
+            return 0, 255, 255
+        case 6:
+            return 255, 255, 0
+
+
 model = models.load_model(Path(__file__).parent / 'model.h5')
 # noinspection PyUnresolvedReferences
 vid = cv2.VideoCapture(0)
@@ -34,8 +53,6 @@ while ret:
         # noinspection PyUnresolvedReferences
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         images.append((Image.fromarray(frame).crop((x, y, x + w, y + h)), x + w, y + h))
-    # noinspection PyUnresolvedReferences
-    cv2.imshow('frame', frame)
     # img = load_img(frame, target_size=(48, 48))
 
     for i in images:
@@ -49,12 +66,13 @@ while ret:
             prediction = model.predict(np.expand_dims(img_arr, axis=0), verbose=False)
             prediction.flatten()
             print(EMOTIONS[int(np.argmax(prediction))])
-            predicted_text = f'{EMOTIONS[int(np.argmax(prediction))]}: {prediction[0]}'
+            predicted_text = f'{EMOTIONS[int(np.argmax(prediction))]}: {np.max(prediction):.4f}'
             # noinspection PyUnresolvedReferences
             cv2.putText(
                 frame, predicted_text, (i[1], i[2]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA
             )
-
+    # noinspection PyUnresolvedReferences
+    cv2.imshow('frame', frame)
     # noinspection PyUnresolvedReferences
     if cv2.waitKey(1) and 0xFF == ord("q"):
         break
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from keras import models
 from keras import layers
 from keras.utils import image_dataset_from_directory
+from keras.callbacks import EarlyStopping
 import pandas as pd
 """
 0 - Angry
@@ -54,12 +55,14 @@ def create_model(_train, _val, _epochs: int = 10, _batch_size: int = 32):
     _model.add(layers.Dense(64, activation='relu'))
     _model.add(layers.Dense(7, activation='softmax'))
     _model.compile(optimizer='rmsprop', metrics=['accuracy'], loss='categorical_crossentropy')
-    _model.fit(_train, epochs=_epochs, validation_data=_val, batch_size=_batch_size)
+    _model.fit(_train, epochs=_epochs, validation_data=_val, batch_size=_batch_size,
+               callbacks=(EarlyStopping(patience=3)))
     return _model
 
 
 if __name__ == '__main__':
     data = get_data()
-    # model = create_model(data[0], data[1], _epochs=5)
-    model = models.load_model('model.h5')
+    model = create_model(data[0], data[1], _epochs=15)
+    # model = models.load_model('model.h5')
+    model.save('model.h5')
     print(model.predict(data[2]))
